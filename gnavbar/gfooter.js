634fcf9e5556c11c4fc6bc84dad358006290593c
rajesh4210 <75352443+rajesh4210@users.noreply.github.com>
2021-11-12 20:51:13 +0530

    Merged Project

---
+++ gnavbar/gfooter.js
@@ -0,0 +1,22 @@
+function footer(){
+    return `
+    <div class=" w-full h-44 bg-gray-100 mt-20">
+        <div class="w-full h-20 flex ">
+            <div class="mt-5 ml-20"> 
+                <p class="text-gray-600">© Powered by smallcase Technologies Pvt. Ltd. Email us at groww-help@smallcase.com</p>
+            </div>
+            <div class="ml-auto space-x-12 mr-20 mt-5">
+                <button class="text-gray-600">Help</button>
+                <button class="text-gray-600">Disclimer</button>
+                <button class="text-gray-600">More</button>
+            </div>
+        </div>
+            <div class="ml-20"> 
+                <img class="h-10 p-1" src="./images/groww-logo.png">
+                <p class="text-gray-600">NSE & BSE – SEBI Reg. No.: INZ0123456789 |  CDSL - SEBI Reg. No.: IN-Dk-117-2019</p>
+            </div> 
+    </div>`
+}
+export default footer
+    
+ 
