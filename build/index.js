406d6b5121a884d09e8f4676013b2305471f46fd
shivam <sap0442@gmail.com>
2021-11-12 21:49:05 +0530

    last touch

---
+++ build/index.js
@@ -0,0 +1,33 @@
+let back = [
+    "../images/back.png",
+    "../images/back2.png",
+];
+
+let front = [
+    "../images/front.png",
+    "../images/front2.png",
+]
+
+let container = document.getElementById("intro_container_in");
+let container2 = document.getElementById("intro_container_in2");
+let interval;
+function startSlideshow() {
+    let counter = 0;
+    container2.style.backgroundImage = `url(${front[counter]})`;
+    container.style.backgroundImage = `url(${back[counter]})`;
+    counter++;
+
+
+    interval = setInterval(function () {
+        if (counter == back.length || counter == front.length) {
+            counter = 0;
+        }
+
+        container2.style.backgroundImage = `url(${front[counter]})`;
+        container.style.backgroundImage = `url(${back[counter]})`;
+        counter++;
+
+    }, 4000);
+}
+
+startSlideshow();
