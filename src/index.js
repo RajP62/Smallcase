de1705775c7ab6775a544bc00412da92f372e600
Shivam Pandey <sap0442@gmail.com>
2021-12-14 21:30:17 +0530

    first commit of authentication

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const blogController = require('./controller/blog.controller');
+const blogController = require('./controllers/blog.controller');
+
+const { register, login } = require("./controllers/auth.controller");
 
 app.use(express.json());
 
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/blog",blogController)
+app.use("/blog", blogController)
+
+app.post("/register",register);
+
+app.post("/login", login);
 
 module.exports = app;
