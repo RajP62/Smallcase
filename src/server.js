f5b227dce8bb1d4493852e72f4b08290194baecb
RajP62 <75352443+rajesh4210@users.noreply.github.com>
2021-12-15 12:58:34 +0530

    all deleted

--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./index');
-const dbConnect = require("./config/db");
-
-app.listen(2000, async()=>{
-    await dbConnect();
-    console.log("Server is running on port 2000");
-});
