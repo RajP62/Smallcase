f68c0276bae113576defebdfd18768e20926ccf1
RajP62 <75352443+rajesh4210@users.noreply.github.com>
2021-12-15 13:06:47 +0530

    Mvc setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const smallcaseController = require("./controller/smallcase.controller");
+const blogController = require('./controllers/blog.controller');
+
+const { register, login } = require("./controllers/auth.controller");
+
+app.use(express.json());
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/blog", blogController)
+
+app.post("/register",register);
+
+app.use("/blog",blogController);
+app.use("/smallcase",smallcaseController)
+app.post("/login", login);
+
+module.exports = app;
